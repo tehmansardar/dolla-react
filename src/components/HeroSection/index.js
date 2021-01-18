f88c367a39de406e72eec0423041c85b0377093e
Tehman Sardar <tehmansardar@gmail.com>
2021-01-18 06:17:59 +0500

    Herosection Design with styled components

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElements';
+import {
+	HeroContainer,
+	HeroBg,
+	VideoBg,
+	HeroContent,
+	HeroH1,
+	HeroP,
+	HeroBtnWrapper,
+	ArrowForward,
+	ArrowRight,
+} from './HeroElements';
+const HeroSection = () => {
+	const [hover, setHover] = useState(false);
+
+	const onHover = () => {
+		setHover(!hover);
+	};
+	return (
+		<HeroContainer>
+			<HeroBg>
+				<VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+			</HeroBg>
+			<HeroContent>
+				<HeroH1>Virtual Bank Made Easy</HeroH1>
+				<HeroP>
+					Sign up for a new account today and recieve $250 in credit towards
+					your next payment.
+				</HeroP>
+				<HeroBtnWrapper>
+					<Button
+						dark='true'
+						primary='true'
+						onMouseEnter={onHover}
+						onMouseLeave={onHover}
+					>
+						Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+					</Button>
+				</HeroBtnWrapper>
+			</HeroContent>
+		</HeroContainer>
+	);
+};
+
+export default HeroSection;
